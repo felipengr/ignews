e906ece23f177edb1bbaab00eb99174ff59e4f7f
Felipe Nogueira <felipenogueira.94@gmail.com>
2022-07-11 21:46:24 -0300

    Gerando previews estáticos

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/react";
 import Head from "next/head";
 import Link from "next/link";
@@ -54,10 +54,12 @@ export default function PostPreview({ post }: PostPreviewProps) {
     );
 };
 
-export const getStaticPaths = () => {
+export const getStaticPaths = async () => {
+
+
     return {
         paths: [],
-        fallback: 'block',
+        fallback: 'blocking',
     }
 }
 
@@ -83,6 +85,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
         props: {
             post,
-        }
+        },
+        redirect: 60 * 30, // 30 minutes
     }
 }
